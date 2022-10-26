1a9e9bcd6872ec4dfd5f1fec8d95c8905982efe5
Sebastiano Secchiaroli <sebssekk@hotmail.it>
2022-10-26 17:03:22 +0200

    organized 'eventReport' component content in the dashboard

--- src/components/eventReport.jsx
+++ src/components/eventReport.jsx
@@ -19,15 +19,32 @@ const EventReport = ({ event }) => {
           aria-controls={`event-${event.id}panel-content`}
           id={`event-${event.id}panel-header`}
         >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+          <div style={{ backgroundColor: "blue" }}>ble</div>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            width="33%"
+            textAlign="start"
+            paddingLeft="25px"
+          >
             {event.title}
           </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            {event.author}
+          <Typography variant="h5" width="33%">
+            {event.place}
+          </Typography>
+          <Typography textAlign="end" width="30%" fontWeight="bold">
+            {event.date}
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>{event.description}</Typography>
+          <Typography textAlign="left" fontWeight="bold">
+            Severity: {event.severity}
+          </Typography>
+          <Typography textAlign="left" fontWeight="bold">
+            Description:
+          </Typography>
+          <Typography padding="0 10%">{event.description}</Typography>
+          <Typography textAlign="end"> &#x2022; {event.author}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
